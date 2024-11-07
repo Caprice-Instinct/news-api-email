38ce4c89371024bc372514909bd1ca4edca4eb97
Caprice-Instinct <linetwangui04@gmail.com>
2024-11-07 22:50:39 +0300

    Add links, specify language, limit news, control category

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import requests
 from send_email import send_email
 
+# Topic to get news of
+topic = "tesla"
+
 # Get API key from newsapi.org
 api_key = "42e505d5f905406ab154e699a57346b4"
 
 # url to get the news on a topic like Tesla; api key attached to it
-url = ("https://newsapi.org/v2/everything?q=tesla"
-       "&from=2024-10-05&sortBy=publishedAt&apiKey"
-       "=42e505d5f905406ab154e699a57346b4")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2024-10-07&sortBy=publishedAt"
+       "&apiKey=42e505d5f905406ab154e699a57346b4&"
+       "language=en")
 
 # A request for content of url
 request = requests.get(url)
@@ -15,11 +19,13 @@ request = requests.get(url)
 content = request.json()
 
 # Empty body for the email
-body = ""
+body = "Subject: Today's news" + "\n"
 # Iterate through the articles; access article titles and descriptions
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+        body = body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2 * "\n"
 
 # Update body into utf-8
 body = body.encode("utf-8")
